81a230f93428844ea0b85358053c4276a9ad1c77
MintyImpetus <user@disp3269>
2023-02-08 10:36:26 -0500

    Added creme colour scheme

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,12 +1,14 @@
 	return require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
 	use { "ellisonleao/gruvbox.nvim" }
+	use {"ntk148v/komau.vim"}
+	use 'pgdouyon/vim-yin-yang'
+	use 'plan9-for-vimspace/acme-colors'
+	use 'huyvohcmc/atlas.vim'
 	use 'junegunn/fzf'
 	use 'junegunn/fzf.vim'
 	use 'junegunn/goyo.vim'
 	use { 'ms-jpq/coq_nvim', branch = 'coq' }
 	use { 'ms-jpq/coq.artifacts', branch = 'artifacts' }
 	use { 'ms-jpq/coq.thirdparty', branch = '3p'}
---	use 'morhetz/gruvbox'
---	use {'prettier/vim-prettier', run = 'yarn install' }
 end)
