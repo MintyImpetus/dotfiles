98e1c9a69388a673302c2698ef9f3f7942bf09fc
MintyImpetus <mi@mimpetus.com>
2023-04-06 15:54:02 +0000

    Removed some unused plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -16,11 +16,11 @@ return require('packer').startup(function(use)
 	use { "ellisonleao/gruvbox.nvim" }
 	use {"ntk148v/komau.vim"}
 	use 'pgdouyon/vim-yin-yang'
-	use 'plan9-for-vimspace/acme-colors'
+--	use 'plan9-for-vimspace/acme-colors'
 	use 'huyvohcmc/atlas.vim'
 	use 'junegunn/fzf'
 	use 'junegunn/fzf.vim'
-	use 'junegunn/goyo.vim'
+--	use 'junegunn/goyo.vim'
 	use { 'ms-jpq/coq_nvim', branch = 'coq' }
 	use { 'ms-jpq/coq.artifacts', branch = 'artifacts' }
 	use { 'ms-jpq/coq.thirdparty', branch = '3p'}
