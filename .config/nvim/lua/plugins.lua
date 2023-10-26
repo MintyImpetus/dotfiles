a9a9c4ba49a633fa7044ae9d8526d44563179a81
MintyImpetus <MintyImpetus@proton.me>
2023-10-26 19:23:04 -0400

    Added plugins for more focussed writing

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -21,7 +21,8 @@ return require('packer').startup(function(use)
 	use 'huyvohcmc/atlas.vim'
 	use 'junegunn/fzf'
 	use 'junegunn/fzf.vim'
---	use 'junegunn/goyo.vim'
+	use 'junegunn/goyo.vim'
+	use 'junegunn/limelight.vim'
 	use { 'ms-jpq/coq_nvim', branch = 'coq' }
 	use { 'ms-jpq/coq.artifacts', branch = 'artifacts' }
 	use { 'ms-jpq/coq.thirdparty', branch = '3p'}
